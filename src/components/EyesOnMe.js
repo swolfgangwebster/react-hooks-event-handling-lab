a0ceb2414b1019db850f83e1f292748ba11a5127
Shawn Webster <swolfgangwebster@gmail.com>
2023-06-25 16:58:04 -0500

    eyes

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { useState} from 'react';
+
+function EyesOnMe() {
+
+    function foc() {
+        console.log("Good!")
+    }
+
+    function blu() {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={foc} onBlur={blu} >Eyes on me</button>
+        </div>
+
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React, { useState} from 'react';
+
 
 function Keypad (){
+    const [ps, setPs] = useState("")
+
+    function handle(e) {
+        console.log("Entering password...")
+        setPs(e.target.value)
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input onChange={handle} type="password" />
+        </div>
     )
 }
 
